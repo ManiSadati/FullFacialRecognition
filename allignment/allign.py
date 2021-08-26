bb29c1cb05ac052e19e160804f964322329b8427
mani <sadati.man@outlook.com>
2021-08-26 16:07:26 +0430

    new allignment with landmarks

--- allignment/allign.py
+++ allignment/allign.py
@@ -1,5 +1,12 @@
 import cv2 as cv
 import numpy as np
+import copy 
+import sys
+sys.path.insert(1, '../recognition/testSphereFace')
+from matlab_cp2tform import get_similarity_transform_for_cv2
+import net_sphere
+import matplotlib.pyplot as plt
+import cv2
 
 def rotateImage(image, angle ):
     image_center = tuple(np.array(image.shape[1::-1]) / 2)
@@ -7,13 +14,14 @@ def rotateImage(image, angle ):
     result = cv.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv.INTER_LINEAR)
     return result
 
-def allign_face(face, xywh, landmarks):
+def allign_face2(face, xywh, landmarks):
     x1 = xywh[0]
     y1 = xywh[1]
     x2 = xywh[2] + x1
     y2 = xywh[3] + y1
     lefteye_x, lefteye_y = landmarks[0], landmarks[1]
     righteye_x, righteye_y = landmarks[2], landmarks[3]
+    nose_x, nose_y = landmarks[4], landmarks[5]
     dx = righteye_x - lefteye_x
     dy = righteye_y - lefteye_y
     angle = np.degrees(np.arctan2(dy, dx))
@@ -21,18 +29,53 @@ def allign_face(face, xywh, landmarks):
     cv.imshow('test' + str(xywh[5]), img)
     return img
 
+
+
+def allign_face(face, h, w,xywh, landmarks):
+    
+    ref_pts = [ [30.2946, 51.6963],[65.5318, 51.5014],[48.0252, 71.7366],[33.5493, 92.3655],[62.7299, 92.2041] ]
+    src_pts = []
+    clors = [(255,0,0),(0,255,0),(0,0,255),(255,255,0),(0,255,255)]
+    x1 = xywh[0]
+    y1 = xywh[1]
+    x2 = xywh[2] + x1
+    y2 = xywh[3] + y1
+
+    for i in range(5):
+        point_x = int(landmarks[2 * i] * w - x1)
+        #print(point_x , " <--> ", landmarks[2*i])
+        point_y = int(landmarks[2 * i + 1] * h - y1)
+        src_pts.append([point_x, point_y])   
+        #face = cv2.circle(face, (point_x, point_y), 2, clors[i], -1)
+    
+
+    #crop_size = (96, 112)
+    crop_size = (96, 112)
+    src_pts = np.array(src_pts).reshape(5,2)
+
+    s = np.array(src_pts).astype(np.float32)
+    r = np.array(ref_pts).astype(np.float32)
+
+    tfm = get_similarity_transform_for_cv2(s, r)
+    face_img = cv2.warpAffine(face, tfm, crop_size)
+    cv.imshow('test' + str(xywh[5]), face_img)
+    return face_img
+
+
 def allign(img, boxes):
     faces = []
+    h, w, c = img.shape
     for box in boxes:
         x1 = box[0]
         y1 = box[1]
         x2 = box[2] + x1
         y2 = box[3] + y1     
         face = img[y1:y2, x1:x2, :]
-        alligned_face = allign_face(face, box, box[6])
-        faces.append(allign_face)
+        alligned_face = allign_face(face, h, w,box, box[6])
+        alligned_face = alligned_face.transpose(2, 0, 1).reshape((1,3,112,96))
+        alligned_face = (alligned_face-127.5)/128.0
+        faces.append(alligned_face)
     return faces
-
 
 
 
